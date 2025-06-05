2d1b518c856cdb2bdffa4284c12fe5203cc0cd2a
Nemo Tamang <nemot54@gmail.com>
2025-06-05 15:34:04 +0545

    detection_model trained. train 4 is complete

--- plate_detection/object_detection_model_train.py
+++ plate_detection/object_detection_model_train.py
@@ -9,8 +9,9 @@ def main():
         data="C:/Users/Nemo/Desktop/license_plate_recognition/plate_detection/config.yaml",
         batch=7, 
         imgsz=640, 
-        epochs=11, 
-        workers=1
+        epochs=1, 
+        workers=1,
+        project="C:/Users/Nemo/Desktop/license_plate_recognition/plate_detection/detect_train"
     )
 
 if __name__ == "__main__":
